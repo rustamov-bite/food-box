970ab50f9d2d2c78c370e551132b31679076c71f
rustamov-bite <assasins_murza006@mail.ru>
2021-03-13 15:17:37 +0600

    init project

---
+++ src/test/java/com/food_box/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_box.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
