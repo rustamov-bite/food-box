a77fc9ec0562c9c819c3f40de38ab15f5ec2fd71
Aishashirin <aishashirina@gmail.com>
2021-03-17 15:27:23 +0600

    create repo for CallBack

---
+++ src/main/java/com/food_box/project/repositories/CallBackRepositories.java
@@ -0,0 +1,7 @@
+package com.food_box.project.repositories;
+
+import com.food_box.project.models.CallBack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CallBackRepositories extends JpaRepository<CallBack,Long> {
+}
